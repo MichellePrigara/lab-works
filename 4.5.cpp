e488e638a6780780ea222e219481c08210aabb6a
MichellePrigara <91586701+MichellePrigara@users.noreply.github.com>
2021-10-19 13:21:41 +0300

    Add files via upload

---
+++ 4.5.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    double a1,b1,a,b,s,d,m,q;
+   std::cin >> a >> b;
+   a1 = abs(a);
+   b1 = abs(b);
+   s = a1+b1;
+   d = a1 - b1;
+   m = a1 * b1;
+   q = a1 * b1;
+   
+   std::cout << s << " " << d << " " << m << " " << q;
+   
+}
