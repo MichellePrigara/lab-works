eeca05070519b185df5cae0ff28af0250f2aa518
MichellePrigara <91586701+MichellePrigara@users.noreply.github.com>
2021-12-27 08:25:58 +0300

    Add files via upload

---
+++ 6.6.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, A1;
+    cin >> A;
+    A1 = A*A;
+    A = A1*A1;
+    A1 = A*A;
+    cout << A1;
+
+    return 0;
+}
