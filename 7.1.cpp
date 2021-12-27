74663bd1d2accebbd68363c9eca4ca01833a0d10
MichellePrigara <91586701+MichellePrigara@users.noreply.github.com>
2021-12-27 09:24:55 +0300

    Add files via upload

---
+++ 7.1.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main()
+{
+    float a;
+    float b;
+    
+    cin >> a;
+    b = a / 180;
+    
+    cout << b;
+    
+    return 0;
+}
