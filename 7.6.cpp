3442bcede06ae576ebe03b8c67879f2f11a240ba
MichellePrigara <91586701+MichellePrigara@users.noreply.github.com>
2021-12-27 09:55:58 +0300

    Add files via upload

---
+++ 7.6.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float D, A1, A2, B1, B2, C1, C2, x, y;
+    
+    cin >> A1 >> B1 >> C1 >> A2 >> B2 >> C2;
+    
+    D = A1*B2 - A2*B1;
+    x = (C1*B2 - C2*B1)/D;
+    y = (A1*C2 - A2*C1)/D;
+    
+    cout << x << "," << y;
+    
+    return 0;
+}
